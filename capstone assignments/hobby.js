3acdc793b288c875c020ff7937fc01aa50a66c79
webdesigner4people <65285895+webdesigner4people@users.noreply.github.com>
2020-07-09 15:15:31 +0530

    Add files via upload

---
+++ capstone assignments/hobby.js
@@ -0,0 +1,13 @@
+function detail1() {
+	 x=document.getElementById("myCarousel");
+	 x.className = "closed"
+  	 
+}
+function detail2() {
+	x=document.getElementById("myCarousel1"):
+	x.className = "closed"
+}
+function detail3() {
+	x=document.getElementById("myCarousel2"):
+	x.className = "closed"
+}
---
+++ capstone assignments/js/portfolioabout.js
@@ -0,0 +1,17 @@
+function closeMe(){
+
+	x=document.getElementById("demo");
+
+	x.className = "closed"
+ }
+function openMe(){
+   		x=document.getElementById("demo");
+    
+   		  x.className="open";
+	}
+
+//when detail1 is open detail 2 and 3 should be close 
+function detail1(){
+	x = document.getElementById("demo");
+	if detail1 == open
+}
